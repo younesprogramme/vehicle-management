42574ddcfd2e40d8ffcca359d9e1216c3d9040b3
you <hilaliyounes.dev@gmail.com>
2024-06-12 10:31:03 +0100

    Bind the repository Interface to the Implementation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\VehicleRepositoryInterface;
+use App\Repositories\VehicleRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(VehicleRepositoryInterface::class, VehicleRepository::class);
     }
 
     /**
